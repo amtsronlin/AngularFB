b2f40f67f7033436ee9621ba0a6306a9d2a4862c
Nham Chi Thanh <Thanh.NhamChi@vn.bosch.com>
2018-12-07 10:47:07 +0700

    add more

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material';
+import { catchError, retry } from 'rxjs/operators';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  url = "/api/login";
+  url = "/api/";
 
   constructor(private router: Router, private http: HttpClient) {}
   username: string;
@@ -22,10 +23,19 @@ export class LoginComponent implements OnInit {
   login(): void {
     const user = { userName: "user", password: "password"};
     console.log(user);
-    this.http.post(this.url, user, {headers: this.headers})
-      .pipe().subscribe(hero => {
+    this.http.post(this.url + "login", user, {headers: this.headers})
+      .pipe(
+      ).subscribe(hero => {
         console.log(hero);
         this.router.navigate(["user"]);
     });
+  }
+
+  user(): void {
+    const user = { userName: "user", password: "password" };
+    this.http.get(this.url + "users", { headers: this.headers })
+      .pipe().subscribe(hero => {
+        console.log(hero);
+      });
   }
 }
